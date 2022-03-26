f23919b24aa115bcbcb798cc497d6d162a390ed6
Greg Benison <gbenison@gmail.com>
2022-03-26 16:31:28 -0700

    Allow customization of docker image in .env

--- cdk/lib/constants.ts
+++ cdk/lib/constants.ts
@@ -5,6 +5,8 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const prefix = (process.env.SUBDOMAIN_PART || 'minecraft').replace(/[^a-zA-Z0-9]/g, '-');
 
+const docker_image = (process.env.DOCKER_IMAGE || 'itzg/minecraft-server');
+
 export const constants = {
   prefix,
   CLUSTER_NAME: prefix,
@@ -15,6 +17,6 @@ export const constants = {
   ECS_VOLUME_NAME: 'data',
   HOSTED_ZONE_SSM_PARAMETER: prefix + 'HostedZoneID',
   LAUNCHER_LAMBDA_ARN_SSM_PARAMETER: prefix + 'LauncherLambdaRoleArn',
-  JAVA_EDITION_DOCKER_IMAGE: 'itzg/minecraft-server',
+  JAVA_EDITION_DOCKER_IMAGE: 'itzg/minecraft-server:java8',
   BEDROCK_EDITION_DOCKER_IMAGE: 'itzg/minecraft-bedrock-server',
 }
